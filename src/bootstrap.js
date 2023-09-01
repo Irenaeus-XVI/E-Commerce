b15ef89aed82268d2a75b039ede9bf88dcf5726a
Ehab-Tarek <ehabtarek166@gmail.com>
2023-09-02 00:22:07 +0300

    Create our own appError class.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,5 @@
 import categoryRoutes from '../src/modules/category/category.routes.js'
+import { AppError } from './utils/AppError.js'
 
 
 export const bootstrap = (app) => {
@@ -7,13 +8,13 @@ export const bootstrap = (app) => {
 
     app.all('*', (req, res, next) => {
         // res.status(404).json('Invalid Path.')
-        next(new Error('Invalid Path.'))
+        next(new AppError('Invalid Path', 404))
     })
 
 
 
     //NOTE - Global Error Handling 
     app.use((err, req, res, next) => {
-        res.status(500).json({ err: err.message })
+        res.status(err.statusCode || 500).json({ err: err.message })
     })
 } 
