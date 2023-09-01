401d8c4b8c15a6236edc7d0f1f12a68ffd60f6da
Ehab-Tarek <ehabtarek166@gmail.com>
2023-09-02 00:08:35 +0300

    HandleAsyncError Function.

--- src/modules/category/controller/category.controller.js
+++ src/modules/category/controller/category.controller.js
@@ -1,41 +1,44 @@
 import { categoryModel } from '../../../../database/models/category.model.js'
 import slugify from "slugify";
+import { handleAsyncError } from '../../../utils/handleAsyncError.js';
 
-const addCategory = async (req, res, next) => {
+const addCategory = handleAsyncError(async (req, res, next) => {
     let { name } = req.body;
     req.body.slug = slugify(req.body.name)
     const category = new categoryModel(req.body)
     await category.save()
     res.status(201).json({ message: "success", category });
-}
+});
 
 
-const getAllCategories = async (req, res, next) => {
+const getAllCategories = handleAsyncError(async (req, res, next) => {
     const categories = await categoryModel.find()
     res.status(201).json({ message: "success", categories });
-}
+});
 
 
 
-const updateCategory = async (req, res, next) => {
+const updateCategory = handleAsyncError(async (req, res, next) => {
     let { id } = req.params;
     let { name } = req.body;
     if (name) req.body.slug = slugify(name)
     const updatedCategory = await categoryModel.findByIdAndUpdate(id, req.body, { new: true })
     res.status(201).json({ message: "success", updatedCategory });
 
-}
+});
 
 
 
-const deleteCategory = async (req, res, next) => {
+const deleteCategory = handleAsyncError(async (req, res, next) => {
     let { id } = req.params;
     const deletedCategory = await categoryModel.findByIdAndDelete(id);
     console.log(deletedCategory);
     !deletedCategory && res.status(404).json({ message: "Category Not Found." });
     deletedCategory && res.status(201).json({ message: "success", deletedCategory });
 
-}
+});
+
+
 export {
     addCategory,
     getAllCategories,
