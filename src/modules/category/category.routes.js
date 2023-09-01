cf2ab8a6eea4ac736b2185cbec809848c6f08b06
Ehab-Tarek <ehabtarek166@gmail.com>
2023-09-01 23:45:13 +0300

    Category Crud.

--- src/modules/category/category.routes.js
+++ src/modules/category/category.routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import * as category from './controller/category.controller.js';
 const router = express.Router();
 
-router.post("/", category.addCategory)
+router.route('/').post(category.addCategory).get(category.getAllCategories)
+router.route('/:id').put(category.updateCategory).delete(category.deleteCategory)
 
-export default router
+export default router 
--- src/modules/category/controller/category.controller.js
+++ src/modules/category/controller/category.controller.js
@@ -1,8 +1,44 @@
 import { categoryModel } from '../../../../database/models/category.model.js'
 import slugify from "slugify";
 
-export const addCategory = async (req, res, next) => {
+const addCategory = async (req, res, next) => {
     let { name } = req.body;
-    const category = await categoryModel.insertMany({ name, slug: slugify(name) });
+    req.body.slug = slugify(req.body.name)
+    const category = new categoryModel(req.body)
+    await category.save()
     res.status(201).json({ message: "success", category });
-}  
+}
+
+
+const getAllCategories = async (req, res, next) => {
+    const categories = await categoryModel.find()
+    res.status(201).json({ message: "success", categories });
+}
+
+
+
+const updateCategory = async (req, res, next) => {
+    let { id } = req.params;
+    let { name } = req.body;
+    if (name) req.body.slug = slugify(name)
+    const updatedCategory = await categoryModel.findByIdAndUpdate(id, req.body, { new: true })
+    res.status(201).json({ message: "success", updatedCategory });
+
+}
+
+
+
+const deleteCategory = async (req, res, next) => {
+    let { id } = req.params;
+    const deletedCategory = await categoryModel.findByIdAndDelete(id);
+    console.log(deletedCategory);
+    !deletedCategory && res.status(404).json({ message: "Category Not Found." });
+    deletedCategory && res.status(201).json({ message: "success", deletedCategory });
+
+}
+export {
+    addCategory,
+    getAllCategories,
+    updateCategory,
+    deleteCategory
+}
