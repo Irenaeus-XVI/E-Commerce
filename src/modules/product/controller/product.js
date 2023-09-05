04ee94b1862625985174227b1d30d45b11723f61
Ehab-Tarek <ehabtarek166@gmail.com>
2023-09-05 22:02:03 +0300

    api features(filtration... )

--- src/modules/product/controller/product.js
+++ src/modules/product/controller/product.js
@@ -15,6 +15,7 @@ const addProduct = handleAsyncError(async (req, res, next) => {
 
 const getAllProducts = handleAsyncError(async (req, res, next) => {
 
+    //NOTE - pagination
     const limit = 5;
     //NOTE - if there is no page given or given string value 
     let page = req.query.page * 1 || 1
@@ -22,7 +23,19 @@ const getAllProducts = handleAsyncError(async (req, res, next) => {
     if (page <= 0) page = 1
     const skip = (page - 1) * limit;
     console.log(page);
-    const products = await productModel.find().skip(skip).limit(limit)
+    //NOTE - filter
+    filtration
+    let filterObg = req.query;
+    let excludedQuery = ['page', 'sort', 'fields', 'keyword'];
+    excludedQuery.forEach(q => {
+        delete filterObg[q];
+    });
+
+    filterObg = JSON.stringify(filterObg);
+    filterObg = filterObg.replace(/(gt|gte|let|lte)/g, match => `$ ${match}`)
+    filterObg = JSON.parse(filterObg);
+    console.log(filterObg);
+    const products = await productModel.find(filterObg).skip(skip).limit(limit)
     res.status(201).json({ page: +page, message: "success", products });
 });
 
