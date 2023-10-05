b905f8ea648042c22e4083f4b55390f71f49c57e
Ehab-Tarek <ehabtarek166@gmail.com>
2023-10-06 02:53:57 +0300

    fix some issues.

--- src/modules/review/review.routes.js
+++ src/modules/review/review.routes.js
@@ -1,17 +1,17 @@
 import express from 'express'
 import * as Review from './controller/review.js';
 import { validation } from '../../middleware/validation.js';
-import { addReviewValidation, deleteReviewValidation, updateReviewValidation } from './Review.validation.js';
+// import { addReviewValidation, deleteReviewValidation, updateReviewValidation } from './Review.validation.js';
 import { allowTo, protectedRoutes } from '../auth/controller/auth.controller.js';
 const router = express.Router();
 
 router.route('/')
-    .post(validation(addReviewValidation), protectedRoutes, allowTo('user'), Review.addReview)
+    .post(protectedRoutes, allowTo('user'), Review.addReview)
     .get(Review.getAllReviews)
 
 router.route('/:id')
-    .put(validation(updateReviewValidation), protectedRoutes, allowTo('user'), Review.updateReview)
-    .delete(validation(deleteReviewValidation), allowTo('admin', 'user'), Review.deleteReview)
+    .put(protectedRoutes, allowTo('user'), Review.updateReview)
+    .delete(allowTo('admin', 'user'), Review.deleteReview)
     .get(Review.getSpecificReview)
 
 export default router  
